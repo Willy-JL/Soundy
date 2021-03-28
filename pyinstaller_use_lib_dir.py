1d52f7703f41c34bb0a33259064aaf58a6ef15df
Willy-JL <willy.leslie@icloud.com>
2021-03-28 22:11:12 +0200

    Test pt2 + PACKAGE

--- pyinstaller_use_lib_dir.py
+++ pyinstaller_use_lib_dir.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.coinit_flags = 0
 sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "lib"))
 import ctypes
 ctypes.windll.kernel32.SetDllDirectoryW(os.path.join(os.path.dirname(sys.argv[0]), "lib"))
