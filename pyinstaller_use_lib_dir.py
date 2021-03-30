249dd529ea13f3f92d93d60a8db6e4556283d365
Willy-JL <willy.leslie@icloud.com>
2021-03-30 13:43:27 +0200

    Fix dll import

--- pyinstaller_use_lib_dir.py
+++ pyinstaller_use_lib_dir.py
@@ -1,5 +1,6 @@
 import os
 import sys
+os.chdir(sys.executable.replace("\\", "/")[:sys.executable.replace("\\", "/").rfind("/")])
 sys.coinit_flags = 0
 sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "lib"))
 import ctypes
